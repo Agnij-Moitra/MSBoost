ae09ad92048225c79839750d0736499d9c79d91c
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2025-04-05 11:55:23 +0530

    mv cython files to ./build

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 import os
 
-# Enable parallel compilation
 os.environ['CYTHON_BUILD_PARALLEL'] = '1'
 
-# Set optimization flags
-extra_compile_args = ['-O3', '-march=native', '-flto']
-extra_link_args = ['-flto']
+extensions = [
+    Extension(
+        "MSBoost",
+        ["MSBoost.pyx"],
+        extra_compile_args=['-O3', '-march=native', '-flto'],
+        extra_link_args=['-flto'],
+        include_dirs=[numpy.get_include()]
+    )
+]
 
 setup(
     ext_modules=cythonize(
-        "MSBoost.pyx",              # Your Cython file
-        annotate=True,              # Optional: generates HTML annotation file
+        extensions,
+        annotate=True,
         compiler_directives={'language_level': '3'},
         build_dir="build",
-        cache=True,                 # Enable caching
-    ),
-    include_dirs=[numpy.get_include()],  # Include NumPy headers if needed
-    extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args,
+        cache=True,
+    )
 )
